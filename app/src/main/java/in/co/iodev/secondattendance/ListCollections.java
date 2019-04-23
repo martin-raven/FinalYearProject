6d5bba8453fa76dc0e4102375027972e800d9e93
martin-raven <martinsiby@gmail.com>
2019-04-23 21:36:16 +0530

    Spinner Fixed

--- app/src/main/java/in/co/iodev/secondattendance/ListCollections.java
+++ app/src/main/java/in/co/iodev/secondattendance/ListCollections.java
@@ -1,5 +1,6 @@
 package in.co.iodev.secondattendance;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
@@ -34,13 +35,13 @@ public class ListCollections extends AppCompatActivity {
     Button button;
     ArrayList<String> classes=new ArrayList<>();
     ArrayAdapter<String> spinnerAdapter;
-
+    Context context;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_list_collections);
-
+        context=this;
         classes.clear();
         classes.add("hello");
         classes.add("hey");
@@ -92,6 +93,10 @@ public class ListCollections extends AppCompatActivity {
                                 list.add(jsonArr.get(i).toString());
                             }
                             classes = list;
+                            spinnerAdapter = new ArrayAdapter(context, android.R.layout.simple_spinner_item, classes);
+                            spinnerAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
+                            spinner.setAdapter(spinnerAdapter);
                             Log.d("SelectClass",classes.toString());
                         } catch (JSONException e) {
                             e.printStackTrace();
